a4cf18e3ab8b27a0f1f66b606d9319d0b4f00cea
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2021-12-11 02:58:48 +0530

    'crying'

---
+++ route/login.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app     = express.Router()
+
+app.get('/login',(req,res)=>{
+    res.render('login')
+})
+
+app.post('/login',(req,res)=>{
+    let data=req.body
+    console.log(data)
+    if(data){
+        if(data.id=='farmer') res.redirect('/farmer')
+        else if(data.id=='customer')res.redirect('/customer')
+        else res.send('Credintial Error') 
+    }
+})
+
+module.exports = app
