0ac1fdaeec58b80d0a888e869af410ab1d7d1fa2
DevadutSB <56481296+DevadutSB@users.noreply.github.com>
2023-02-11 23:25:17 +0530

    err_handle

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const fileUpload      = require('express-fileupload');
-const express         = require('express')
-const ExifImage       = require('exif').ExifImage
-const { promisify }   = require('util')
-const write           = promisify(require('fs').writeFile)
+const fileUpload    = require('express-fileupload');
+const express       = require('express')
+const ExifImage     = require('exif').ExifImage
+const bodyParser = require('body-parser')
+
 
 const PORT = process.env.PORT || 3030;
-let url = ''
+let pos,er='';
 
 const app = express();
 app.set('view engine', 'ejs')
@@ -13,96 +13,117 @@ app.set('view engine', 'ejs')
 function cal(lant){
     return lant[0] + (lant[1]/60) + (lant[2]/3600)
 }
+
+function verify(latlon){
+    return (latlon[0] + latlon[1] + latlon[2])!=0
+}
+
 function getUrl(gps){
-    lat = gps.GPSLatitude
-    lon = gps.GPSLongitude
-    return `https://www.google.com/maps/@${cal(lat)},${cal(lon)},100m/data=!3m1!1e3`
+    let lat    = gps.GPSLatitude
+    let lon    = gps.GPSLongitude
+    let cordD  = `${lat[0]}°${lat[1]}'${lat[2]}\"${gps.GPSLatitudeRef},${lon[0]}°${lon[1]}'${lon[2]}\"${gps.GPSLongitudeRef}`
+    let cordA  = `${cal(lat)},${cal(lon)}`
+    return {
+        cordA:cordA,
+        cordD:cordD
+    }
 }
 
 app.use(fileUpload());
- 
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/index.html")
+    res.render('body',{
+        zoom:14,
+        cordA:'47.50737,19.04611',
+        cordD:'',
+        er:"No files uploaded"
+    })
 })
  
 app.post('/upload', function (req, res) {
+    
     let sampleFile;
     let uploadPath;
  
     if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
+        res.render('body',{
+            zoom:14,
+            cordA:'47.50737,19.04611',
+            cordD:'',
+            er:"No files uploaded"
+        })
     }
- 
+  
     sampleFile = req.files.file;
     uploadPath = __dirname + '/uploads/' + sampleFile.name;
  
     sampleFile.mv(uploadPath, function(err){
-        if (err)
-            return res.status(500).send(err);
- 
+        if (err){
+            res.render('body',{
+                zoom:14,
+                cordA:'47.50737,19.04611',
+                cordD:'',
+                er:err
+            })
+        }
         try {
-            new ExifImage({ image : __dirname + '/uploads/' + sampleFile.name }, async(error, exifData)=>{
-                if (error)
-                res.send('Error: '+error.message);
-                else{
-                    if(exifData.gps.GPSLatitude && exifData.gps.GPSLatitude){
-                        url = getUrl(exifData.gps)
-                    }
-                    else{
-                        url=''
-                    }
-                    await write('map.html',`
-                        <!DOCTYPE html>
-                        <html lang="en">
-                        <head>
-                            <meta charset="UTF-8">
-                            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                            <title>Map</title>
-                        </head>
-                        <body>
-                        <script>
-                            function verify(obj){
-                                return (obj[0] + obj[1] + obj[2]) == 0
-                            }
-
-                            let exif = ${JSON.stringify(exifData)}
-                            let url = '${url}'
-                            
-                            if(url.length>51){
-                                alert("Allow Popup to see the map")
-                                window.open(url)
-                            }
-                            else{
-                                if(exif){
-                                    if(!exif.gps.GPSLatitude && !exif.gps.GPSLatitude){
-                                        alert("EXIF GPS Data is empty")
-                                    }
-                                    else if(verify(exif.gps.GPSLongitude) && verify(exif.gps.GPSLatitude)){
-                                        alert("While taking this image, GPS Access  was given to camera but GPS was off")
-                                    }
-                                    else{
-                                        alert("EXIF GPS info is not present in image")
-                                    }
-                                }
-                                else{
-                                    alert("Image doesnot contain EXIF data")
-                                }
-                                
-                            }
-                        </script>
-                        </body>
-                        </html>
-                    `)
-                    await new Promise(function(resolve) { 
-                        setTimeout(resolve, 1000)
-                    });
-                    res.sendFile(__dirname + '/map.html')
+            new ExifImage({ image : __dirname + '/uploads/' + sampleFile.name }, async(er, exifData)=>{
+                if (er){
+                    er="EXIF data is corroupted or source of the image is social media platforms like facebook , twitter etc. They delete EXIF data due to privacy concerns"
+                    res.render('body',{
+                        zoom:14,
+                        cordA:'47.50737,19.04611',
+                        cordD:'',
+                        er:er
+                    })
                 }
-                    // Do something with your data!
+                else{
+                    if(!exifData.gps){
+                        er = "GPS info not found"
+                        res.render('body',{
+                            zoom:14,
+                            cordA:'47.50737,19.04611',
+                            cordD:'',
+                            er:er
+                        })
+                    }
+                    if(!exifData.gps.GPSLongitude || !exifData.gps.GPSLatitude){
+                        er = "GPS Longitude or GPS Latitude Missing"
+                        res.render('body',{
+                            zoom:14,
+                            cordA:'47.50737,19.04611',
+                            cordD:'',
+                            er:er
+                        })
+                    }
+                    if(!verify(exifData.gps.GPSLongitude) && !verify(exifData.gps.GPSLatitude)){
+                        er = "GPS accecs  was given to camera,but GPS was off while capturing this"
+                        res.render('body',{
+                            zoom:14,
+                            cordA:'47.50737,19.04611',
+                            cordD:'',
+                            er:er
+                        })
+                    }
+                    if(verify(exifData.gps.GPSLatitude) && verify(exifData.gps.GPSLatitude)){
+                        console.log(exifData)
+                        pos = getUrl(exifData.gps)
+                        res.render('body',{
+                            zoom:req.body.zoom,
+                            cordA:pos.cordA,
+                            cordD:pos.cordD,
+                            er:''
+                        })
+                    }
+                }
             });
-        } catch (error) {
-            console.log('Error: ' + error.message);
+           
+           
+        } catch (er) {
+            console.log('er: ' + er.message);
         }
     });
 });
